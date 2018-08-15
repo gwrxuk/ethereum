01958de12ec200c40cd001fea8e23bc018b252e7
gwrxuk <gwrx2005@gmail.com>
2018-08-15 09:56:04 +0800

    Create SimpleContract.sol

---
+++ SimpleContract.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.0;
+contract SampleContract {
+    uint storageData;
+    function set(uint x) {
+        storageData = x;
+    }
+    function get() constant returns (uint) {
+        return storageData;
+    }
+}
